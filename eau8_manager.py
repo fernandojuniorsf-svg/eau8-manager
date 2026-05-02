7a8019081f41d5a8893805ba824f9e1d2e0e11f3
fernandojuniorsf-svg <fernando.juniorsf@gmail.com>
2026-05-02 20:31:45 -0300

    Update eau8_manager.py

--- eau8_manager.py
+++ eau8_manager.py
@@ -853,8 +853,6 @@ elif menu == "Validacao por Foto (IA)":
 elif menu == "Enviar por WhatsApp":
     st.markdown("### Enviar por WhatsApp")
     pywhatkit_ok = False
-    try:
-    pywhatkit_ok = False
     tab1, tab2, tab3, tab4 = st.tabs(["Enviar Escala", "Mensagem Personalizada", "Mensagens Rapidas", "Contatos Salvos"])
     with tab1:
         st.markdown("#### Enviar Ultima Escala")
